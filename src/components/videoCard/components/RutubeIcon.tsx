4bdd92e016a2bc3ae1bb11b68b9c8d934b2cb7a0
Aleksey Yamkin <me@jamkin.ru>
2026-09-12 21:09:16 +0300

    Remove leftover duplicate RutubeIcon from videoCard folder

--- src/components/videoCard/components/RutubeIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Image from 'next/image';
-
-const RutubeIcon: React.FC = () => <Image src="/Icon_rutube.svg" alt="Rutube" width={24} height={24} className="w-6 h-6" />;
-
-RutubeIcon.displayName = 'RutubeIcon';
-
-export default React.memo(RutubeIcon);
